90304b84ad8f04c0d11e7b43667a5d516102037e
andytgarcia <andytgarcia27@gmail.com>
2022-12-06 10:50:12 -0800

    create map

---
+++ Player.py
@@ -0,0 +1,38 @@
+import pygame
+
+
+class Player:
+    def __init__(self, screen, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        pygame.draw.circle(self.screen, pygame.Color(0, 0, 255), (self.x, self.y), 7.5)
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.moveUp()
+        if keys[pygame.K_s]:
+            # moveDown
+            self.moveDown()
+        if keys[pygame.K_a]:
+            # moveLeft
+            self.moveLeft()
+        if keys[pygame.K_d]:
+            # moveRight
+            self.moveRight()
+
+    def moveUp(self):
+        self.y = self.y - 5
+
+    def moveDown(self):
+        self.y = self.y + 5
+
+    def moveLeft(self):
+        self.x = self.x - 5
+
+    def moveRight(self):
+        self.x = self.x + 5
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import pygame
 
+from Player import *
+
+
+def clearScreen():
+    pygame.draw.rect(screen, pygame.Color(0, 0, 0), (0, 0, 1280, 720))
+
+
+def drawMap():
+    pygame.draw.rect(200, 500, 320, 300)
 
 
 # start of program
@@ -8,10 +17,16 @@ FPS = 60  # 60 frames per second
 fpsClock = pygame.time.Clock()
 screen = pygame.display.set_mode((1280, 720))
 gameOver = False
+player = Player(screen, 640, 360)
 
 while not gameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOver = True
+
+    clearScreen()
+    player.draw()
+    player.movement()
+
     pygame.display.flip()
     fpsClock.tick(FPS)

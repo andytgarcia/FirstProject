7958bc7ff92e197504f89235912ad7077efc3926
andytgarcia <andytgarcia27@gmail.com>
2022-12-06 20:55:02 -0800

    too many bugs in the code

--- Player.py
+++ Player.py
@@ -6,6 +6,7 @@ class Player:
         self.screen = screen
         self.x = x
         self.y = y
+    
 
     def draw(self):
         pygame.draw.circle(self.screen, pygame.Color(0, 0, 255), (self.x, self.y), 7.5)
@@ -36,3 +37,14 @@ class Player:
 
     def moveRight(self):
         self.x = self.x + 5
+        
+    def getSize():
+        return 7.5
+    
+    
+    def getPlayerHurtBox(self):
+        return pygame.Rect(self.x - 7.5, self.y - 7.5, 7.5 * 2, 7.5 * 2)
+    
+    
+    
+    
